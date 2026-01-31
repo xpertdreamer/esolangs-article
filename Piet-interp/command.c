8709c8cbc1bdf292316ce3d09afefcc9d27a386a
xpertdreamer <yungnomen@gmail.com>
2026-01-31 21:19:03 +0300

    Fix command module

--- Piet-interp/command.c
+++ Piet-interp/command.c
@@ -9,170 +9,117 @@
 #include <stdlib.h>
 
 bool piet_execute_cmd(PietCommand cmd, Stack *stack, int block_size) {
-    int64_t a, b, c, res;
+    int64_t a, b, res;
 
     switch (cmd) {
-        case P_CMD_PUSH: {
+        case P_CMD_PUSH:
             return st_push(stack, block_size);
-        }
 
-        case P_CMD_POP: {
-            return st_pop(stack);
-        }
+        case P_CMD_POP:
+            return st_pop(stack) != 0;  // Just pop, don't care about value
 
-        case P_CMD_ADD: {
+        case P_CMD_ADD:
             a = st_pop(stack);
             b = st_pop(stack);
-            if (!a || !b)
-                return false;
-            res = a + b;
+            res = b + a;
             return st_push(stack, res);
-        }
 
-        case P_CMD_SUB: {
+        case P_CMD_SUB:
             a = st_pop(stack);
             b = st_pop(stack);
-            if (!a || !b)
-                return false;
-            res = b -a;
+            res = b - a;
             return st_push(stack, res);
-        }
 
-        case P_CMD_MUL: {
+        case P_CMD_MUL:
             a = st_pop(stack);
             b = st_pop(stack);
-            if (!a || !b)
-                return false;
-            res = a * b;
+            res = b * a;
             return st_push(stack, res);
-        }
 
-        case P_CMD_DIV: {
+        case P_CMD_DIV:
             a = st_pop(stack);
             b = st_pop(stack);
-            if (!a || !b)
-                return false;
             if (a == 0) {
                 PIET_ERROR("Division by zero!\n");
                 return false;
             }
             res = b / a;
             return st_push(stack, res);
-        }
 
-        case P_CMD_MOD: {
+        case P_CMD_MOD:
             a = st_pop(stack);
             b = st_pop(stack);
-            if (!a || !b)
-                return false;
             if (a == 0) {
                 PIET_ERROR("Modulo by zero!\n");
                 return false;
             }
             res = b % a;
             return st_push(stack, res);
-        }
 
-        case P_CMD_NOT: {
+        case P_CMD_NOT:
             a = st_pop(stack);
-            if (!a)
-                return false;
             res = (a == 0) ? 1 : 0;
             return st_push(stack, res);
-        }
 
-        case P_CMD_GREATER: {
+        case P_CMD_GREATER:
             a = st_pop(stack);
             b = st_pop(stack);
-            if (!a || !b)
-                return false;
             res = (b > a) ? 1 : 0;
             return st_push(stack, res);
-        }
 
-        case P_CMD_POINTER: {
+        case P_CMD_POINTER:
             a = st_pop(stack);
-            if (!a)
-                return false;
-            // I think it will be better handled by interpreter
+            // Let interpreter handle this
             return true;
-        }
 
-        case P_CMD_SWITCH: {
+        case P_CMD_SWITCH:
             a = st_pop(stack);
-            if (!a)
-                return false;
-            // Also handled by interpreter
+            // Let interpreter handle this
             return true;
-        }
 
-        case P_CMD_DUPL: {
+        case P_CMD_DUPL:
+            if (st_is_empty(stack)) return false;
             a = st_peek(stack);
-            if (!a)
-                return false;
             return st_push(stack, a);
-        }
 
-        case P_CMD_ROLL: {
+        case P_CMD_ROLL:
             a = st_pop(stack);
             b = st_pop(stack);
-            if (!a || !b)
-                return false;
             return st_roll(stack, b, a);
-        }
 
         case P_CMD_IN_NUM: {
             int num;
             if (scanf("%d", &num) != 1) {
-                PIET_ERROR("Failed to read number input!\n");
                 return false;
             }
             return st_push(stack, num);
         }
 
         case P_CMD_IN_CHAR: {
-            int ch;
-            fflush(stdout);
-            ch = getchar();
-            if (ch == '\n') {
-                ch = getchar();
-            }
-            if (ch == EOF) {
-                PIET_ERROR("Unexpected end of input!\n");
-                return false;
-            }
+            int ch = getchar();
+            if (ch == EOF) return false;
             return st_push(stack, ch);
         }
 
-        case P_CMD_OUT_NUM: {
+        case P_CMD_OUT_NUM:
             a = st_pop(stack);
-            if (!a)
-                return false;
-            printf("%lld\n", (long long)a);
+            printf("%lld", (long long)a);
             fflush(stdout);
             return true;
-        }
 
-        case P_CMD_OUT_CHAR: {
+        case P_CMD_OUT_CHAR:
             a = st_pop(stack);
-            if (!a)
-                return false;
             if (a >= 0 && a <= 255) {
                 putchar((char)a);
                 fflush(stdout);
-            } else {
-                PIET_ERROR("Invalid char value: %lld\n", (long long)a);
-                return false;
+                return true;
             }
-            return true;
-        }
+            return false;
 
-        case P_CMD_NONE: {
+        case P_CMD_NONE:
             return true;
-        }
 
         default:
-            PIET_ERROR("Unknown command: %d\n", cmd);
             return false;
     }
 }
